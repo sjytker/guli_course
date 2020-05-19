8b81f3bdd2f08bd78e2efd9afe38b9d1ec6ef772
Suujy <401839797@qq.com>
2020-05-19 22:48:38 +0800

    first commit

---
+++ service/service_edu/src/main/java/com/example/demo/edu/controller/TeacherController.java
@@ -0,0 +1,21 @@
+package com.example.demo.edu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-05-19
+ */
+@RestController
+@RequestMapping("/edu/teacher")
+public class TeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/example/demo/edu/service/TeacherService.java
@@ -0,0 +1,16 @@
+package com.example.demo.edu.service;
+
+import com.example.demo.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-05-19
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
