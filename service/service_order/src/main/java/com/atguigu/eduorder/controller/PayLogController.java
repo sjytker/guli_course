c61834dca85fdb69b2f428a1635cfa66ec1624d7
Suujy <401839797@qq.com>
2020-11-14 22:51:53 +0800

    fix bug

--- service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
@@ -49,7 +49,7 @@ public class PayLogController {
             payLogService.updateOrdersStatus(map);
             return R.ok().message("支付成功");
         }
-        return R.ok().code(25000).message("支付中");
+        return R.ok().Code(25000).message("支付中");
 
     }
 }
